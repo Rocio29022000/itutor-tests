dad03cb90ef65112d4c193d70c6e4cc35a045810
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-07-03 14:55:46 +0100

    Started the JS

---
+++ JavaScript/billie.js
@@ -0,0 +1,10 @@
+"use strict"
+
+//FOR NOW: Construct Fill in questions
+function Fill(questionsNum, questionName, qMin, qMax) {
+    this.questionsNum = questionsNum;
+    this.questionName = questionName;
+    this.qMin = qMin;
+    this.qMax = qMax;
+    this.render();
+  }
--- JavaScript/rocio.js
+++ JavaScript/rocio.js
@@ -1,1 +1,25 @@
 "use strict"
+console.log("Hi!")
+
+
+//empty array to push new questions when created
+const questions = [];
+
+//Random number to select what question is shown
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+// Constructor function to create new Question
+// with each question I want to create a question area and an answer area.
+
+function MCQ(questionsNum, questionName, qMin, qMax) {
+    this.questionsNum = questionsNum;
+    this.questionName = questionName;
+    this.qMin = qMin;
+    this.qMax = qMax;
+    this.render();
+  }
+
+
+  
