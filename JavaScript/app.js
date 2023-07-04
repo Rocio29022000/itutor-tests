d497aaa5911af1789d0b01dfd1be0e5ddce185dd
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-07-04 13:33:29 +0100

    now renders either from fill or mcq

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,15 +1,76 @@
 "use strict"
-console.log("This is te OG")
+console.log("This is the OG")
 
 //Target elements
 const questionName = document.getElementById("question-area");
 const questionAnswer = document.getElementById("answer-area");
 const question = document.getElementById("question");
 
+//empty array to push new questions when created
+let allQuestions = [];
+let questions = ["01", "02"]
+
 //Random Number generator CHECK IF QUESTIONS OR ALL QUESTIONS FOR THE LENGTH
-function randomNum(){
-    return Math.floor(Math.random() * allQuestions.length);
+function randomNum(min, max){
+    // return Math.floor(Math.random() * allQuestions.length);
+    return Math.floor(Math.random()*questions.length);
+
 }
+
+//Constructor function for new object (MCQ question)
+function MCQ(questionName, src, alt) {
+    this.questionName = questionName;
+    this.src = src;
+    this.alt = alt;
+    allQuestions.push(this)
+  }
+
+  //Construct Fill in questions
+function Fill(questionName, src, alt) {
+    this.questionName = questionName;
+    this.src = src;
+    this.alt = alt;
+    allQuestions.push(this)
+  }
+
+
+// Create new instances of an object (create new question)
+
+
+
+function renderMCQ(){
+    let q1 = randomNum();
+    console.log("This is MCQ " + q1);
+    question.src = allQuestions[q1].src;
+    question.alt = allQuestions[q1].alt;
+}
+
+function renderFill(){
+    let q1 = randomNum();
+    console.log("This is Fill-in " + q1);
+    question.src = allQuestions[q1].src;
+    question.alt = allQuestions[q1].alt;
+  
+  }
+
+function which(){
+    let mcqOrFill = randomNum();
+    console.log(mcqOrFill)
+    if (mcqOrFill == 1){
+        for (let i=0; i < questions.length; i++){
+            new MCQ(questions[i], `/Assets/Maths/c1/Easy/MCQ/${questions[i]}.png`);
+        }
+        renderMCQ();
+    } else {
+        for (let i=0; i < questions.length; i++){
+            new Fill(questions[i], `/Assets/Maths/c1/Easy/Fill/${questions[i]}.png`);
+          }
+        renderFill();
+    }
+}
+which();
+
+
 
 
 
