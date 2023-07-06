5876bb117d87da955949d6bad6a1f5fb4bfd6421
BILLIEBAXTER <hello.billiebaxter@gmail.com>
2023-07-06 13:03:24 +0100

    new colors and button code

--- JavaScript/billie.js
+++ JavaScript/billie.js
@@ -1,27 +1,64 @@
 "use strict";
 
-// Wait for document to load
-  document.addEventListener("DOMContentLoaded", function(event) {
-  document.documentElement.setAttribute("data-theme", "light");
-      
-// Get our button switcher
-  let themeSwitcher = document.getElementById("theme-switcher");
-      
-// When our button gets clicked
-  themeSwitcher.onclick = function() {
-// Get the current selected theme, on the first run
-// it should be `light`
-  let currentTheme = document.documentElement.getAttribute("data-theme");
-      
-// Switch between `dark` and `light`
-  let switchToTheme = currentTheme === "dark" ? "light" : "dark"
-      
-// Set our current theme to the new one
-  document.documentElement.setAttribute("data-theme", switchToTheme);
-          }
-        }
-        );
+let darkMode = false;
 
+// add event listeners to the buttons
+document.getElementById("darkButton").addEventListener("click", enterDarkMode);
+
+document.getElementById("lightButton").addEventListener("click", enterLightMode);
+
+//  create a dark mode function
+function enterDarkMode() {
+  let body = document.body;
+  let iTutor = document.getElementById("iTutor");
+  let button = document.getElementById("darkButton");
+  body.classList.remove("light");
+  iTutor.classList.remove("light");
+  body.classList.add("dark");
+  iTutor.classList.add("dark");
+  button.setAttribute("checked", "checked");
+  // local storage
+  darkMode = true;
+  // console.log(darkMode);
+  saveMode();
+}
+
+function enterLightMode() {
+  let body = document.body;
+  let iTutor = document.getElementById("iTutor");
+  let button = document.getElementById("lightButton");
+  body.classList.remove("dark");
+  iTutor.classList.remove("dark");
+  body.classList.add("light");
+  iTutor.classList.add("light");
+  button.setAttribute("checked", "checked");
+  //local storage
+  darkMode = false;
+  // console.log(darkMode);
+  saveMode();
+}
+
+function saveMode() {
+  let mode = JSON.stringify(darkMode);
+  localStorage.setItem("darkMode", mode);
+}
+
+function pageLoad() {
+  let storedMode = JSON.parse(localStorage.getItem("darkMode"));
+  // console.log(storedMode);
+  if (storedMode === null) {
+    console.log("null");
+    return;
+  }
+  if (storedMode === false) {
+    console.log("light");
+    enterLightMode();
+  } else if (storedMode) {
+    console.log("dark");
+    enterDarkMode();
+  }
+}
+pageLoad();
 
   // let questionSection1 = prompt ("")
   //   if (questionSection1 === "2" && questionSection2 === "4" && questionSection3 = "c") 
